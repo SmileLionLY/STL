1196a88acf9565a4b0b081f3faa1d5c9ae826b4a
liuyihpc <liuyihpc@sensetime.com>
2018-03-21 09:28:54 +0800

    add queue

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -4,10 +4,24 @@
 #include <string>
 
 void deque_create_add_remove() {
-       
-    
+    std::deque<int> deque_int{1, 2, 3};
+    std::queue<int> queue_int(deque_int);
+    std::queue<int> queue_int1(queue_int);
+    int a = 0;
+    queue_int.push(a);
+    queue_int.push(1);
+    queue_int.emplace(2);
+    while(!queue_int.empty()) {
+        std::cout << "queue : " << queue_int.front() << std::endl;
+        queue_int.pop();
+    }
+    queue_int.swap(queue_int1);
+    while(!queue_int.empty()) {
+        std::cout << "queue : " << queue_int.front() << std::endl;
+        queue_int.pop();
+    }
 }
 
 int main() {
-    
+    deque_create_add_remove();
 }
